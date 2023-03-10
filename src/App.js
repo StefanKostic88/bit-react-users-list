a049086669e872ddb371382eb69cefdd431a7ed9
StefanKostic88 <stefankostic88@yahoo.com>
2023-03-10 15:12:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Pages/Root";
 import HomePage from "./Pages/HomePage";
 import CardOverview from "./Pages/CardOverview";
+import { useEffect, useState } from "react";
 
 const dataFils = require("./assets/usersData/usersData.json");
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    setUsersData(() => dataFils.results);
+  }, []);
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
       children: [
-        { path: "/", element: <HomePage data={dataFils.results} /> },
+        { path: "/", element: <HomePage data={usersData} /> },
+
         {
           path: "/cardoverview",
-          element: <CardOverview data={dataFils.results} />,
+          element: <CardOverview data={usersData} />,
         },
       ],
     },
